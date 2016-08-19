809cc6b052d7d589acc03d31dfa0f98fabaf2199
Yuri47 <yuri.alexs@gmail.com>
2016-08-19 18:17:07 -0300

    add mayer bitch

--- app/Http/Controllers/addUser.php
+++ app/Http/Controllers/addUser.php
@@ -12,7 +12,7 @@ class addUser extends Controller
     //
 public function index(Request $request) {
 	DB::table('users')->insert(
-    ['name'=>'John Bitch','email' => 'john@example.com' ]
+    ['name'=>'Mayer Bitch','email' => 'mayer@example.com' ]
 );
 
 	var_dump($request -> all());

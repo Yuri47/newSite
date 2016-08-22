0c2add12aad7a597e43a1e931c61376a27405b8e
Yuri Alexsander <yuri.alexs@gmail.com>
2016-08-21 22:12:55 -0300

    testes com upload de img

--- app/Http/Controllers/addUser.php
+++ app/Http/Controllers/addUser.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use DB;
-
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 class addUser extends Controller
 {
     //
@@ -15,8 +17,12 @@ public function index(Request $request) {
 
 $name = $request -> input('name');
 $email = $request -> input('email');
-
-
+$destinationPath = 'image/';
+ 
+         $request->file('image')->move($destinationPath, 'novoArquivo.jpg');
+             
+          //  return redirect('/form')->with('status', 'Su imagen de perfil ha sido cambiada con éxito');
+         
 
 
 //inserir no banco de dados
--- resources/views/form/form.blade.php
+++ resources/views/form/form.blade.php
@@ -2,10 +2,13 @@
 @section('title', 'Formulario')
 
 @section('conteudo')
-<form method="post" action="/addUser">
+<form method="post" action="/addUser" enctype="multipart/form-data">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     Nome: <input type="text" name="name" required><br>
-    Email: <input type="text" name="email" required>
+    Email: <input type="text" name="email" required><br>
+
+    Imagem: <input type="file" name="image"> <br>
+    <div class='text-danger'>{{$errors->first('image')}}</div>
     <input type="submit" value="Enviar">
 
 </form>
